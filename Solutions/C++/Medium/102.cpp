381155842ef2c6bcbbed27a910213f6560ef55a7
Diego Noguera Maciel <53942598+Dieg0Maciel@users.noreply.github.com>
2023-03-06 16:25:36 -0300

    Create 102.cpp

---
+++ Solutions/C++/Medium/102.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    std::vector<std::vector<int>> levelOrder(TreeNode* root) {
+        std::vector<vector<int>> result;
+        if (!root) {
+            return result;
+        }
+        std::deque<TreeNode*> q;
+        q.push_back(root);
+        while (q.size() > 0) {
+            int qSize = q.size();
+            std::vector<int> levelNodes;
+            while (qSize > 0) {
+                TreeNode* node = q.front();
+                q.pop_front();
+                qSize--;
+                levelNodes.push_back(node->val);
+                if (node->left) {
+                    q.push_back(node->left);
+                }
+                if (node->right) {
+                    q.push_back(node->right);
+                }
+            }
+            if (levelNodes.size() > 0) {
+                result.push_back(levelNodes);
+            }
+        }
+        return result;
+    }
+};

94ca4acb2856ae96a776ccfe0d474c795a922805
Diego Noguera Maciel <53942598+Dieg0Maciel@users.noreply.github.com>
2023-03-09 10:02:08 -0300

    Create 144.cpp

---
+++ Solutions/C++/Easy/144.cpp
@@ -0,0 +1,23 @@
+/*
+    Create a fast node and a slow node to traverse the list, if there is a cycle both 
+    nodes will meet at the end of the list, just before the cycle begins.
+*/
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if (!head) {
+            return false;
+        }
+        ListNode* fast = head;
+        ListNode* slow = head;
+        while (fast && fast->next) {
+            fast = fast->next->next;
+            slow = slow->next;
+            if (fast == slow) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
